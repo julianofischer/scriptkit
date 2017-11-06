75c2b8e73eea015cb5fb76108683bd02c3d30700
Juliano Naves <julianofischer@gmail.com>
2017-11-06 12:04:09 -0400

    time filtering example

--- time_filtering.py
+++ time_filtering.py
@@ -1,16 +1,24 @@
+# @julianofischer
+# use mode: python3 time_filtering.py trace_file begin_time final_time
+
 import sys
 
-with open(sys.argv[1], 'r') as test_cases:
-    for test in test_cases:
-        test = test.rstrip()
-        time = int(test.split()[0])
-        final_time = int(sys.argv[2])
-        '''action = test[1]
-        from_node = test[2]
-        to_node  = test[3]
-        info = test[4]'''
-        
-        if time > final_time:
-            break
-        
-        print (test)
+assert 3 <= len(sys.argv) <= 4, "Illegal number of arguments"
+
+begin_time = int(sys.argv[2])
+end_time = -1
+if len(sys.argv) == 4:
+    end_time = int(sys.argv[3])
+
+with open(sys.argv[1], 'r') as trace_file, open(sys.argv[1]+"_output",'+w') as output_file:
+    for line in trace_file:
+        line = line.rstrip().split()
+        time = int(line[0])
+
+        if time >= begin_time:
+            if time > end_time > -1:
+                break
+
+            time = time - begin_time
+            line[0] = str(time)
+            output_file.write("\t".join(line)+"\n")
